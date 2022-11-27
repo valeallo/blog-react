bbf96c74353d5e8ee395228eef6e9c3411f66ba5
valeallo <valentina.allodi@gmail.com>
2022-11-28 00:00:15 +0100

    xx

--- src/Components/BlogPost.js
+++ src/Components/BlogPost.js
@@ -3,12 +3,27 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { insertInFavorite, favorite, resetFavorite } from '../States/favoriteSlice'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { useState, useEffect } from 'react';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 const BlogPost = ({ post }) => {
-    const dispatch = useDispatch()
-    
+    const dispatch = useDispatch()    
     const AllFavorites = useSelector(favorite)
+    const [isFavorite, setFavorite] = useState(false)
 
+
+    const addFavorite = () => {
+        dispatch(insertInFavorite(post))
+        console.log(AllFavorites)
+    }
+    useEffect( () =>{ 
+        
+        if (AllFavorites.includes(post)){
+            setFavorite(true)
+        }
+        else{ setFavorite(false)}
+    }, [addFavorite])
+    
     
     return (
         <div className="flex flex-col bg-[#ffffff] max-w-[300px] max-h-[300px] rounded-md hover:scale-105 hover:shadow-xl">
@@ -28,11 +43,6 @@ const BlogPost = ({ post }) => {
                     </div>
 
                     <button className='p-2 bg-red-200'
-                        onClick={() => {
-                            
-                            dispatch(insertInFavorite(post))
-                            console.log(AllFavorites)
-                        }}
                     >
                         favs
                     </button>
@@ -42,11 +52,12 @@ const BlogPost = ({ post }) => {
                     </small>
                 </div>
                 <div className="flex flex-row justify-between w-full">
-                    <FavoriteBorderIcon onClick={() => {
+                <div onClick={() => {
                             
-                            dispatch(insertInFavorite(post))
-                            console.log(AllFavorites)
-                        }}/>
+                            
+                        }}>    
+                {isFavorite? <FavoriteIcon/> : <FavoriteBorderIcon />}
+                </div>
                     <Link
                         to={`/posts/${post._id}`}
                         className="text-xs text-[#916BBF] underline"
