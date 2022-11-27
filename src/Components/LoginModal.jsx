a1ba467becaeaea86f5e2bf990f8dc15405dbe02
valeallo <valentina.allodi@gmail.com>
2022-11-27 23:38:36 +0100

    xx

--- src/Components/LoginModal.jsx
+++ src/Components/LoginModal.jsx
@@ -6,6 +6,7 @@ import { loggedInUser, user } from '../States/loginSlice'
 
 const LoginModal = ({ close }) => {
     const [formData, setFormData] = useState({})
+    const url = `${process.env.REACT_APP_SERVER_BASE_URL}/login`
     const data = {
         email: formData.email,
         password: formData.password
@@ -44,6 +45,9 @@ const LoginModal = ({ close }) => {
                             </div>
                             <form
                                 className="mt-8 space-y-6"
+                                action=''
+                                method='POST'
+                                onSubmit={loginUser}
                             >
                                 <div className="-space-y-px rounded-md shadow-sm">
                                     <div>
@@ -102,7 +106,7 @@ const LoginModal = ({ close }) => {
 
                                 <div>
                                     <button
-                                        onClick={loginUser}
+                                        type='submit'
                                         className="group relative flex w-full justify-center rounded-md border border-transparent bg-[#4e38b3] py-2 px-4 text-sm font-medium text-white hover:bg-[#3D2C8D] focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                                     >
 
